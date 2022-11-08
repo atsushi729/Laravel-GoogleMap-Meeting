81d3b6121f6d36d1c9cde4bce31bcaf72f3d496f
Atsushi <atu729@icloud.com>
2022-11-08 23:40:35 +0900

    added transaction for UpdateUsecase.php

--- src/app/Usecases/Chirps/UpdateUsecase.php
+++ src/app/Usecases/Chirps/UpdateUsecase.php
@@ -4,6 +4,7 @@ namespace App\Usecases\Chirps;
 
 use App\Http\Controllers\Controller;
 use App\Http\Payload;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class UpdateUsecase extends Controller
@@ -11,11 +12,15 @@ class UpdateUsecase extends Controller
     public function run($request, $chirp)
     {
         try {
+            DB::beginTransaction();
+            $this->authorize('update', $chirp);
             $validated = $request->validated();
-            $request->user()->chirps()->update($validated);
+            $chirp->update($validated);
+            DB::commit();
 
         } catch (\Exception $e) {
             Log::error($e);
+            DB::rollBack();
             return (new Payload())->setStatus('FAILED');
         }
         return (new Payload())->setStatus('SUCCESS');
