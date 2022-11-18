9a44db8bd04589bfe957aa960ac40a6cfabb31ba
Atsushi <atu729@icloud.com>
2022-11-18 23:45:58 +0900

    fixed web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,12 +13,4 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware(['auth', 'verified'])->group(function() {
-    Route::get('/chirps/index', \App\Http\Actions\Chirps\IndexAction::class)->name('chirps.index');
-    Route::post('/chirps/store', \App\Http\Actions\Chirps\StoreAction::class)->name('chirps.store');
-    Route::get('/chirps/{chirp}', \App\Http\Actions\Chirps\EditAction::class)->name('chirps.edit');
-    Route::patch('/chirps/{chirp}', \App\Http\Actions\Chirps\UpdateAction::class)->name('chirps.update');
-    Route::delete('chirps/{chirp}', \App\Http\Actions\Chirps\DeleteAction::class)->name('chirps.destroy');
-});
-
 require __DIR__.'/auth.php';
